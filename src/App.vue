32db51d1e09bc982dd58214b53327f8e739d9deb
Petr Chlapec <chlapec.petr@feg.eu>
2026-04-21 10:21:42 +0200

    Practice end

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue'
+const categories = ['Food', 'Transport', 'Entertainment', 'Bills', 'Other']
+
+const expenses = ref([])
+const description = ref('')
+const amount = ref('')
+const category = ref(categories[0])
+
+const addExpense = () => {
+  if (description.value.trim() === '' || amount.value <= 0) return
+  expenses.value.push({
+    id: Date.now(),
+    description: description.value,
+    amount: amount.value,
+    category: category.value,
+  })
+  description.value = ''
+  amount.value = ''
+  category.value = categories[0]
+}
+
+const removeExpense = (id) => {
+  expenses.value = expenses.value.filter((expense) => expense.id !== id)
+}
+
+const totalAmount = computed(() => {
+  return expenses.value.reduce((sum, expense) => sum + expense.amount, 0)
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Expense Tracker</h1>
+  <h2>Total amount: {{ totalAmount }} CZK</h2>
+  <h3 v-if="expenses.length > 0">
+    You logged {{ expenses.length }} {{ expenses.length === 1 ? 'expense' : 'expenses' }}
+  </h3>
+  <input type="text" v-model="description" />
+  <input type="text" v-model.number="amount" />
+  <select name="category" id="category" v-model="category">
+    <option v-for="cat in categories" :value="cat" :key="cat">{{ cat }}</option>
+  </select>
+  <button @click="addExpense">Add</button>
+  <ul>
+    <li v-for="expense in expenses" :key="expense.id">
+      <span>{{ expense.description }}: {{ expense.amount }} CZK</span><br />
+      <span>{{ expense.category }}</span
+      ><br />
+      <button @click="removeExpense(expense.id)">Remove</button>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
